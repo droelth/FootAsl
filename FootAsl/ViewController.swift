62812ca64a7663ae458a9f99cb1065fe8f007ef3
Berkay <berkaysutlu94@gmail.com>
2023-01-13 00:42:36 +0300

    Initial Commit

---
+++ FootAsl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FootAsl
+//
+//  Created by Berkay Sutlu on 13.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
